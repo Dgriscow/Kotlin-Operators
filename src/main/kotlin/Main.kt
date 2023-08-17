f92fe16f004e15a8591f6a7e0506390a0764d9f9
Dgriscow <dgrischow@students.mchenry.edu>
2023-08-17 17:00:01 -0500

    finnished

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    val a = 5
+    val b = 6
+    val c = 10
+
+    val x = a + a
+    println("$a + $a = $x")
+    val y = b - c
+    println("$b - $c = $y")
+    val z = a * y
+    println("$a * $y = $z")
+    val j = x / y
+    println("$x / $y = $j")
+    val k = j % a
+    println("$j % $a = $k")
+    val l = k.inc()
+    println("$k ++ = $l")
+
+
+}
